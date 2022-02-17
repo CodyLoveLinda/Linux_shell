5eb1784de727b09016b656d9da778deeddca4a68
mingmingzhang <mingmingzhang@autox.ai>
2022-02-17 09:56:53 +0800

    add sed

--- find_string_in_txt/find_and_sed.sh
+++ find_string_in_txt/find_and_sed.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# sed -i "s/tf_user/tf_user_index/g" user.sql 
 #sed -i '/echo_red "Error, please check logs."/a \ \ \ exit 1' camer.pb.txt
 #num=1
 #sed -i '1i drink tea' camer.pb.txt
@@ -12,14 +13,14 @@ do
     #fi
     vat=`echo $line | grep '/image/'` #找到包含的字符串
     if [ -n "$vat" ]; then
-	echo $line
+	#echo $line
 	line=`echo $line | sed '/^$/d'` #去掉空格
     	line=`echo ${vat%/*} | sed  "s/image/image_header/g"` #替换字符串
 	echo $line
 	NUM=$[$NUM + $vsr1]
 	#sed '$1i drink tea'
-    else
-	    echo $line
+    #else
+	    #echo $line
     fi
 	#echo $line
     #echo $line | sed -e "/image"
