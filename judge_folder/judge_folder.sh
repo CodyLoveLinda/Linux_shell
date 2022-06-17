abbcc8f74bb3417113ae2d841d7c9cd5cedd8b0e
mingmingzhang <mingmingzhang@autox.ai>
2022-06-17 09:59:13 +0800

    add judge file

--- judge_folder/judge_folder.sh
+++ judge_folder/judge_folder.sh
@@ -5,3 +5,17 @@ if [ -d "Top/" ]; then
 else
 	echo "no folder"
 fi
+
+  camera_irq_dir="$HOME/platsw/kernel/drivers/acash3"
+  if [ -d "$camera_irq_dir" ]; then
+    sudo autoxmount -m
+    cp /autox-sz/mmzhang_cash/acash3_irq_bind_core.sh $camera_irq_dir/
+  fi
+
+  camera_irq_file="$HOME/platsw/kernel/drivers/acash3/acash3_irq_bind_core.sh"
+  if [ -f "$camera_irq_file" ]; then
+      #echo "$camera_irq_file is exit"
+      bash $camera_irq_file > camera_irq_log
+  else
+      info "$camera_irq_file is not exit"
+  fi
