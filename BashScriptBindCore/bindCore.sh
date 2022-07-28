c3e00f33d0a0afa92b08bbc85c5fe954894022a3
mingmingzhang <mingmingzhang@autox.ai>
2022-07-28 16:38:50 +0800

    add bash bind core

--- BashScriptBindCore/bindCore.sh
+++ BashScriptBindCore/bindCore.sh
@@ -4,4 +4,4 @@ MIN_CPU_FOR_CHROME=$(( $(nproc) -4 ))
 MAX_CPU_FOR_CHROME=$(( $(nproc) -1 ))
 CPUSETS="$MIN_CPU_FOR_CHROME-$MAX_CPU_FOR_CHROME"
 
-taskset -p $CPUSETS ./home/mingmingzhang/project/linux_shell/BashScriptBindCore/cycleRun.sh &
+taskset -c 3 ./cycleRun.sh &
