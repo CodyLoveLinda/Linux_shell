7ae4d1e80cddbac1fe526835de678e63a2770ae1
mingmingzhang <mingmingzhang@autox.ai>
2022-09-08 14:22:09 +0800

    add plot processor
    add cpu run 100% script

--- cpu/plot_process.py
+++ cpu/plot_process.py
@@ -1,6 +1,7 @@
 import re
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 
 def is_number(s):
     try:
@@ -18,20 +19,22 @@ def is_number(s):
  
     return False
 
-def main():
-    file = open('20220812-143731process138707.txt','r')
+def main(filename):
+    print(filename)
+    file = open(filename,'r')
     list = []
     # search the line including accuracy
     for line in file:
-        if len(line) == 75 :
+        #print(len(line))
+        if len(line) == 85 :
             #print(len(line))
             line = ' '.join(line.split())
             #print(line)
             #str_line = line.replace(' ', '')
             str_list = line.split(" ")
-            print(str_list[6])
-            if is_number(str_list[6]) :
-                list.append(float(str_list[6]))
+            #print(str_list[3])
+            if is_number(str_list[3]) :
+                list.append(float(str_list[3]))
             #str_list_2 = str_list[0].split(":")
             #if str_list_2[0] == "%Cpu0" :
             #    str_list_3 = str_list_2[1].split("u")
@@ -47,4 +50,8 @@ def main():
     plt.show()
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str)
+    args = parser.parse_args()
+    print(args.filename)
+    main(args.filename)
